5e14f42920b1c84a803d6b38a630f4750842897f
Alfiya Kamalova <kamalova.alfiya@list.ru>
2023-02-20 20:44:00 +0500

    conditions

---
+++ Desktop/L1_C#/examples/example005_conditions/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "тагир")
+{
+    Console.WriteLine("Ура, это же Тагир! Люблю тебя!");
+}
+else
+{
+    Console.Write("Привет, обычный человек ");
+    Console.WriteLine(username);
+}
+
